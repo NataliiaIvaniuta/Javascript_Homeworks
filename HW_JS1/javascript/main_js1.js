23cbb022049959577b7137d6c03e14e74a98f625
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-06 12:37:40 +0300

    Add files via upload

---
+++ HW_JS1/javascript/main_js1.js
@@ -0,0 +1,20 @@
+"use strict";
+let access= ("Welcome, ");
+let confirmation = "Are you sure you want to continue?";
+let forbidden ="You are not allowed to visit this website:(";
+let yourName = prompt ("What is your name?");
+let yourAge = prompt ("How old are you?", 18);
+if (yourAge<18) {
+    alert (forbidden);
+} else if (yourAge >= 18 && yourAge <= 22) {
+    if (yourAge >= 18 && yourAge <= 22) {
+    } if (confirm(confirmation)) {
+        alert(access + yourName);
+    } else {
+        alert(forbidden);
+    }
+}
+else {
+    alert(access + yourName);
+}
+
