9bd77d1c5d8c6113c1bc6921d787181fef6c407f
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-09 01:40:16 +0300

    Update main_js3.js

--- HW_JS3/javascript/main_js3.js
+++ HW_JS3/javascript/main_js3.js
@@ -1,13 +1,13 @@
 "use strict";
-let firstNum=+parseInt (prompt("Please enter the number #1", 0));
+let firstNum=parseInt (prompt("Please enter the number #1", 0));
 let operation = prompt("Select an operation sign", '+');
-let secondNum=+parseInt (prompt ("Please enter the number #2", 0));
+let secondNum=parseInt (prompt ("Please enter the number #2", 0));
 
 while(isNaN(firstNum)) {
-    firstNum=+parseFloat(prompt('Please enter the number #1', firstNum));
+    firstNum=parseInt(prompt('Please enter the number #1', firstNum));
 }
 while(isNaN(secondNum)) {
-    secondNum=+parseFloat(prompt('Please enter the number #2', secondNum));
+    secondNum=parseInt(prompt('Please enter the number #2', secondNum));
 }
 while (operation!=="+"&& operation!=="-"&& operation!=="*" && operation!=="/") {
     operation = prompt("Select a correct operation sign", operation);
