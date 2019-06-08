7ed9858d52c61357a711b1181271fc625ebeaf00
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-09 01:37:29 +0300

    Update main_js3.js

--- HW_JS3/javascript/main_js3.js
+++ HW_JS3/javascript/main_js3.js
@@ -1,38 +1,31 @@
 "use strict";
-let firstNum=parseInt (prompt("Please enter the number #1", 0));
+let firstNum=+parseInt (prompt("Please enter the number #1", 0));
 let operation = prompt("Select an operation sign", '+');
-let secondNum=parseInt (prompt ("Please enter the number #2", 0));
+let secondNum=+parseInt (prompt ("Please enter the number #2", 0));
 
 while(isNaN(firstNum)) {
-    firstNum=parseFloat(prompt('Please enter the number #1', firstNum));
+    firstNum=+parseFloat(prompt('Please enter the number #1', firstNum));
 }
 while(isNaN(secondNum)) {
-    secondNum=parseFloat(prompt('Please enter the number #2', secondNum));
+    secondNum=+parseFloat(prompt('Please enter the number #2', secondNum));
 }
 while (operation!=="+"&& operation!=="-"&& operation!=="*" && operation!=="/") {
     operation = prompt("Select a correct operation sign", operation);
 }
-
-function add (firstNum, secondNum){
-    return (`${firstNum} + ${secondNum} = ${firstNum + secondNum}`)}
-function divide (firstNum, secondNum){
-    return (`${firstNum} / ${secondNum} = ${firstNum / secondNum}`)}
-function multiply(firstNum, secondNum){
-    return (`${firstNum} * ${secondNum} = ${firstNum * secondNum}`)}
-function subtract(firstNum, secondNum){
-    return (`${firstNum} - ${secondNum} = ${firstNum - secondNum}`)}
-
-switch (operation) {
-    case '+':
-        console.log (add(firstNum, secondNum));
-        break;
-    case '-':
-        console.log (subtract(firstNum, secondNum));
-        break;
-    case '*':
-        console.log (multiply(firstNum, secondNum));
-        break;
-    case '/':
-        console.log (divide(firstNum, secondNum));
-        break;
+function calculate() {
+    let num1=firstNum;
+    let num2=secondNum;
+    let oper=operation;
+    switch (oper) {
+        case '+':
+            return (num1 + num2);
+        case '-':
+            return (num1 - num2);
+        case '*':
+            return (num1 * num2);
+        case '/':
+            return (num1 / num2);
+    }
 }
+console.log (calculate(firstNum, secondNum, operation));
+
