d9a1f2bac916157e84efa932d2c859ee9aadb027
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-17 15:28:15 +0300

    Update main_js6.js

--- HW_JS6/javascript/main_js6.js
+++ HW_JS6/javascript/main_js6.js
@@ -1,12 +1,8 @@
 "use strict";
 
 function filterBy(someArray, filterType){
-    let filteredArray=someArray.filter(function(el){
-        return typeof (el) !== typeof(filterType)
-    });
-    return filteredArray
-};
+    return someArray.filter((el)=> typeof el !== filterType)}
 
-console.log(filterBy(["some", null, "words", 123, "11", null, 55, NaN], "string"));
-console.log(filterBy(["some", null, "words", 123, "11", null, 55, NaN], null));
-console.log(filterBy(["some", null, "words", 123, "11", null, 55, NaN], NaN));
+console.log(filterBy(["some", null, "words", 123, "11", null, 55, NaN], typeof "string"));
+console.log(filterBy(["some", null, "words", 123, "11", null, 55, NaN], typeof null)); //for another type of data
+console.log(filterBy(["some", null, "words", 123, "11", null, 55, NaN], typeof NaN)); //for another type of data
