808919c46ece52cdf5c250836b61a06eb0754353
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-17 15:02:18 +0300

    Update main_js5.js

--- HW_JS5/javascript/main_js5.js
+++ HW_JS5/javascript/main_js5.js
@@ -1,31 +1,35 @@
 "use strict";
+
 function createNewUser() {
-    this.firstName = prompt("What is your name?");
-    while (this.firstName === '') {
-        this.firstName = prompt('Please specify your name');
-    }
-    this.lastName = prompt("What is your surname?");
-    while (this.lastName === '') {
-        this.lastName = prompt('Please specify your surname')
-    }
-    this.birthday = prompt('When is your birthday (dd.mm.yyyy)?').split(".");
-    while (this.birthday === '') {
-        this.birthday =prompt('Please, specify when is your birthday (dd.mm.yyyy)').split(".");
-    }
-    this.getAge= function () {
-        return parseInt((new Date() - Date.parse((this.birthday.reverse()))) / (24 * 3600 * 365.25 * 1000));
+    let firstName = myCustomPrompt("What is your name?");
+    let lastName = myCustomPrompt("What is your surname?");
+    let birthday = myCustomPrompt('When is your birthday (dd.mm.yyyy)?');
+
+    this.getAge = function () {
+        return parseInt((new Date() - Date.parse(((birthday.split(".")).reverse()))) / (24 * 3600 * 365.25 * 1000));
     };
     this.getLogin = function () {
-        return this.firstName.charAt(0).toLowerCase() + this.lastName.toLowerCase();
+        return firstName.charAt(0).toLowerCase() + lastName.toLowerCase();
     };
     this.getPassword = function () {
-        return this.firstName.charAt(0).toUpperCase() + this.lastName.toLowerCase() + this.birthday[2];
+        return firstName.charAt(0).toUpperCase() + lastName.toLowerCase() + (birthday.split("."))[2];
     };
 }
 
-let newUser = new createNewUser();
+function myCustomPrompt(ask) {
+    let input;
+    do {
+        if (!input) {
+            input = prompt(ask)
+        }
+    }
+        while (!(input));
+    return input;
+};
 
-console.log(newUser);
-console.log(newUser.getLogin());
-console.log(newUser.getPassword());
-console.log(newUser.getAge());
+    let newUser = new createNewUser();
+
+    console.log(newUser);
+    console.log(newUser.getLogin());
+    console.log(newUser.getPassword());
+    console.log(newUser.getAge());
