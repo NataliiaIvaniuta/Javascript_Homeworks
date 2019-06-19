df9160f492108a70a1b5e09f69992596b7ea92f8
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-19 16:00:59 +0300

    Add files via upload

---
+++ HW_JS7/javascript/main_js7.js
@@ -0,0 +1,38 @@
+'use strict';
+//style
+document.body.style.backgroundColor='pink';
+document.getElementById("timer").style.fontSize = '30px';
+document.body.style.fontSize='20px';
+document.body.style.textIndent='20px';
+
+function showArrayList (someArray) {
+    let arrList = [];
+
+    for (let i = 0; i < someArray.length; i++) {
+        arrList[i] = someArray[i];
+    }
+    console.log(arrList);
+
+    const mapArray = `<ul> ${arrList.map(item => `<li> ${item}</li>`)} </ul>`;
+
+    let splitAndJoin = mapArray.split(',').join(' ');
+    document.getElementById('container').innerHTML = splitAndJoin;
+}
+
+let sec = 10;
+function countDown() {
+    if (sec < 1) {
+        document.body.innerHTML = '';
+    }
+    else {
+        document.getElementById('timer').innerHTML = sec;
+        setTimeout(countDown, 1000);
+        sec--;
+    }
+}
+(countDown());
+
+// showArrayList(['Paris', 'London', 'Kiev', 'Tokyo', 'Moscow', 'Rome']);
+showArrayList(['train', 'bus', 'plane', 'boat', 'car', [1, '2', 3]]);
+
+//не получилось - "Если внутри массива одним из элементов будет еще один массив или объект, выводить его как вложенный список."
