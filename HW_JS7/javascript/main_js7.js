1869dbee0715c4f51ec9a07b5509cfc538bae743
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-20 23:42:15 +0300

    Update main_js7.js

--- HW_JS7/javascript/main_js7.js
+++ HW_JS7/javascript/main_js7.js
@@ -1,16 +1,30 @@
 'use strict';
-//style
+
 document.body.style.backgroundColor='pink';
 document.getElementById("timer").style.fontSize = '30px';
 document.body.style.fontSize='20px';
 document.body.style.textIndent='20px';
 
 function showArrayList (someArray) {
-    const mapArray = `<ul> ${someArray.map(item => `<li> ${item}</li>`)} </ul>`;
-
-    let splitAndJoin = mapArray.split(',').join(' ');
-    document.getElementById('container').innerHTML = splitAndJoin;
+    const mapArray = someArray.map(item => {
+        if (typeof (item) === "object") {
+            return showArrayList(item);
+        } else {
+            return `<li>${item}</li>`;
+        }
+    });
+    return `<ul>${mapArray.join(' ')}</ul>`;
 }
+    document.getElementById('container').innerHTML = showArrayList(['train', 'bus', 'plane', 'boat', 'car', [1, '2', 3]]);
+
+
+//1 variant
+// function showArrayList (someArray) {
+//     const mapArray = `<ul> ${someArray.map(item => `<li> ${item}</li>`)} </ul>`;
+//
+//     let splitAndJoin = mapArray.split(',').join(' ');
+//     document.getElementById('container').innerHTML = splitAndJoin;
+// }
 
 let sec = 10;
 function countDown() {
@@ -26,6 +40,4 @@ function countDown() {
 (countDown());
 
 // showArrayList(['Paris', 'London', 'Kiev', 'Tokyo', 'Moscow', 'Rome']);
-showArrayList(['train', 'bus', 'plane', 'boat', 'car', [1, '2', 3]]);
-
-//не получилось - "Если внутри массива одним из элементов будет еще один массив или объект, выводить его как вложенный список."
+// showArrayList(['train', 'bus', 'plane', 'boat', 'car', [1, '2', 3]]);
