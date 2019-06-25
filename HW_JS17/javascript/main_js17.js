1b8f8df182b85753c482ddd22450d4e464af0159
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-25 23:43:36 +0300

    Update main_js17.js

--- HW_JS17/javascript/main_js17.js
+++ HW_JS17/javascript/main_js17.js
@@ -3,19 +3,27 @@ let n = +prompt("Please enter a number");
 while (n === undefined || isNaN(n)) {
     n = +prompt("Please enter a valid number", n);
 }
+console.log(n);
 
-alert (fibonacci(n));
-
-function fibonacci(n) {
-     if (n > 1) {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    } else if (n<0) {
-        return fibonacci(n + 2) - fibonacci(n + 1);
-    } else {
-        return n;
+function fibonacci (F0, F1, n) {
+    if (n ===0) {
+        F2=F0;
+        return F2;
     }
+    if (n===1) {
+        F2=F1;
+        return F2;
+    }
+    else {
+        if (n < 0) {
+        F2=fibonacci(F0, F1, n+2)-fibonacci (F0, F1, n+1);
+    } else {
+        F2=fibonacci (F0, F1, n-1)+fibonacci (F0, F1, n-2);
+    }
+        return F2
 }
+}
+let F2=0;
 
-//Formulas
-// Fn = Fn − 1 + Fn − 2   //for positive numbers
-// F{n}=F{n+2}-F{n+1}     // for negative numbers
+let result=fibonacci(1, 2, n);
+console.log(result);
