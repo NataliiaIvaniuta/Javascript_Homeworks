df9f4dfbf93305bc12b43d9744fcceda93b6b0b2
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-06-29 00:08:58 +0300

    Update main_js21.js

--- HW_JS21/javascript/main_js21.js
+++ HW_JS21/javascript/main_js21.js
@@ -1,83 +1,48 @@
 'use strict';
+function makeString (obj) {
+    return JSON.stringify(obj).toLowerCase();
+};
 
-function filterCollection(arrayObj, keyWords, flag) {
-    let filtereredArr = [];
-    let indexFilter = 0;
-    let checkFilter = 0;
-    let arrayKeys = [];
-    let indexArrayKeys = 0;
-    for (let i = 3; i < arguments.length; i++) {
-        arrayKeys[indexArrayKeys] = arguments[i];
-        indexArrayKeys++;
+function filterCollection(array, keyWords, flag) {
+    if (typeof keyWords !== "string") {
+        keyWords = String(keyWords);
     }
-
-    let arrayWords = keyWords.split(" ");
-    let arrayWordsIs = [];
-
-
-    for (let n = 0; n < arrayObj.length; n++) {
-        for (let p = 0; p < arrayWords.length; p++) {
-            arrayWordsIs[p] = 0;
-        }
-        for (let i = 0; i < arrayWords.length; i++) {
-            for (let j = 0; j < arrayKeys.length; j++) {
-                let isSubKey = arrayKeys[j].indexOf(".");
-                if (isSubKey === -1) {
-                    if (arrayKeys[j] in arrayObj[n]) {
-                        if (arrayObj[n][arrayKeys[j]].indexOf(arrayWords[i]) >= 0) {
-                            arrayWordsIs[i]++;
-                        }
-                    }
+    keyWords = keyWords.trim();
+    let arrKeyWord = keyWords.split(" ").filter((item) => {
+        return item !== "";
+    });
+    arrKeyWord = arrKeyWord.map((item) => {
+        return item.toLowerCase();
+    });
+    let arr = [];
+        if (flag) {
+            arr = array.filter((item) => {
+                if (typeof item === 'object') {
+                    let str = makeString(item);
+                    return arrKeyWord.every((e) => {
+                        return str.includes(e);
+                    })
                 } else {
-                    let mainKey = arrayKeys[j].substring(0, isSubKey);
-                    let subKey = arrayKeys[j].substring(isSubKey + 1);
-                    if (mainKey in arrayObj[n]) {
-                        arrayWordsIs[i] = checkSubObject(arrayObj[n][mainKey], subKey, arrayWords[i], arrayWordsIs[i]);
-                    }
+                    return arrKeyWord.every((e) => {
+                        return item.toLowerCase().includes(e);
+                    });
                 }
-            }
-        }
-        for (let i = 0; i < arrayWordsIs.length; i++) {
-            if (arrayWordsIs[i] > 0) {
-                checkFilter++;
-            }
-        }
-
-        if (flag === true && checkFilter === arrayWordsIs.length) {
-            filtereredArr[indexFilter] = arrayObj[n];
-            indexFilter++;
-        } else if (flag === false && checkFilter > 0) {
-            filtereredArr[indexFilter] = arrayObj[n];
-            indexFilter++;
-        }
-        checkFilter = 0;
-    }
-    return filtereredArr;
-}
-
-function checkSubObject(subObj, subKey, keyWord, currentCount) {
-    let countWord = currentCount;
-    let checkPoint = subKey.indexOf(".");
-    if (checkPoint === -1) {
-        if (!Array.isArray(subObj)) {
-            if (subKey in subObj) {
-                if (subObj[subKey].indexOf(keyWord) >= 0) {
-                    countWord++;
-                }
-            }
+            });
         } else {
-            for (let i = 0; i < subObj.length; i++) {
-                countWord = checkSubObject(subObj[i], subKey, keyWord, countWord);
-            }
+            arr = array.filter((item) => {
+                if (typeof item === 'object') {
+                    let str = makeString(item);
+                    return arrKeyWord.some((e) => {
+                        return str.includes(e);
+                    })
+                } else {
+                    return arrKeyWord.some((e) => {
+                        return item.toLowerCase().includes(e);
+                    });
+                }
+            });
         }
-    } else {
-        let mainKey = subKey.substring(0, checkPoint);
-        let addKey = subKey.substring(checkPoint + 1);
-        if (mainKey in subObj) {
-            countWord = checkSubObject(subObj[addKey], addKey, keyWord, countWord);
-        }
-    }
-    return countWord;
+    return arr;
 }
 
 let vehicles = [
@@ -120,5 +85,5 @@ let vehicles = [
 ];
 
 console.log(vehicles);
-console.log(filterCollection(vehicles, "apa atch", false, 'name', 'country', 'engine.liquid'));
+console.log(filterCollection(vehicles, "gas ch", true, 'name', 'country', 'engine.liquid'));
 
