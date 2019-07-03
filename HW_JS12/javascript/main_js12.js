24c9c480e3a3fc2eca19721490199664d5869cb8
NataliiaIvaniuta <50379495+NataliiaIvaniuta@users.noreply.github.com>
2019-07-03 18:02:50 +0300

    Update main_js12.js

--- HW_JS12/javascript/main_js12.js
+++ HW_JS12/javascript/main_js12.js
@@ -1,21 +1,17 @@
 'use strict';
 
-//to start automatically
-// window.addEventListener('load', counter);
 // creating panel
 const panel=document.createElement('div');
 const stop=document.createElement('button');
   stop.innerText='Stop';
 const resume=document.createElement('button');
   resume.innerText='Resume';
-// const timer=document.createElement('span');
-//   timer.style.color='red';
+
 //adding panel
   document.querySelector('.images-wrapper').before(panel);
   panel.appendChild(stop);
   panel.appendChild(resume);
-  // panel.appendChild(timer);
-
+ 
 let progressFlag=true;
 let images = Array.from(document.querySelector('.images-wrapper').children);
 let i=0;
@@ -24,8 +20,7 @@ let i=0;
     let slider = setInterval(()=> {
         if (progressFlag) {
             images.forEach((el) => {
-                // el.classList.toggle('fade');
-                el.setAttribute('hidden', 'true');
+               el.setAttribute('hidden', 'true');
             });
         images[i].removeAttribute('hidden');
         (i === images.length - 1) ? i = 0 : i++;
@@ -41,12 +36,3 @@ panel.addEventListener('click', (event)=> {
         progressFlag=true;
     }
 });
-//
-//   ///timer settings
-// let slideInterval;
-// let timerCount;
-// //
-// let countdown=()=>{
-//     let timeLeft=9
-//
-// }
